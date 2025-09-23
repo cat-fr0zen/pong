36ab8955a9503a3d9626a22181374a2d7dc8047b
Ducret matthéo <mattheo.ducret@etu.unicaen.fr>
2025-09-23 14:59:19 +0200

    ajout de corps de fonction javascript et du début html

---
+++ pong.js
@@ -0,0 +1,14 @@
+var raquette1 = document.getElementById("joueurréel")
+var raquette2 = document.getElementById("joueurbot")
+
+function reninitialiser_partie(){
+
+}
+
+function lancer_partie(){
+
+}
+
+function deplacement_raquette(){
+    
+}
